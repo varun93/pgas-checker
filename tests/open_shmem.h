efb3f887f1c4d08759bf976e8a6cff8e2788cbbb
Varun <1hvarun@gmail.com>
2019-10-04 11:26:42 -0400

    Added prototype for shmem_free

--- tests/open_shmem.h
+++ tests/open_shmem.h
@@ -1,6 +1,6 @@
 // dummy declarations
 void shmem_put(void *dest, void *source, int nelems, int pe);
 void shmem_get(void *dest, void *source, int nelems, int pe);
+void shmem_free(void *ptr, int pe);
 void *shmem_malloc(int size);
 void *shmem_barrier_all();
-
