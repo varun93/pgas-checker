e823c07fedf8c3386da4f281b218913c047042e7
varun93 <1hvarun@gmail.com>
2019-11-10 10:46:56 -0500

    cleaned up the process of invoking default handlers

--- checker_extension_ideation/checker.cpp
+++ checker_extension_ideation/checker.cpp
@@ -2,6 +2,12 @@
 // include your checker declartion
 #include "my_checker.h"
 
+// doesn't work in c++11 standard
+// Please refer to the following post
+// https://stackoverflow.com/questions/18837857/cant-use-enum-class-as-unordered-map-key 
+
+typedef std::unordered_map<Routine, Handler> defaultHandlers;
+defaultHandlers defaults;
 
 // memory allocation routine
 void defaultAlloc() {
@@ -28,44 +34,59 @@ void defaultRead() {
 	std::cout << "Default Read\n";
 } 
 
+// get the default handler
+Handler getDefaultHandler(Routine routineType) {
+
+    defaultHandlers::const_iterator iterator = defaults.find(routineType);
+
+    if(iterator != defaults.end()) {
+        return iterator->second; 
+    }
+
+    return (Handler)NULL;
+}
 
 void event_handler(std::string routineName) {
 
     // usage; this is how it would be used in the main checker
     routineHandlers::const_iterator iterator = handlers.find(routineName);
+    Handler routineHandler = NULL;
 
     if(iterator != handlers.end()) {
     	std::string routineName = iterator->first;
     	Pair value = iterator->second;
     	Routine routineType = value.first;
-    	Handler routineHandler = value.second;
-    	// if the event handler exists invoke it; else call the default implementations
-    	if(routineHandler != NULL) {
-    		routineHandler();
-    	}
-    	else{
-    		switch(routineType) {
-    			case MEMORY_ALLOC: 
-	    			defaultAlloc();
-	    			break;
-	    		case MEMORY_DEALLOC: 
-		    		defaultDealloc();
-		    		break;
-		    	case SYNCHRONIZATION:
-		    		defaultSynchronization();
-		    		break;
-		    	case WRITE_TO_MEMORY:
-			    	defaultWrite();
-			    	break;
-			    case READ_FROM_MEMORY:
-			    	defaultRead();
-			    	break;
-    		}
-    	}
+        // if the event handler exists invoke it; else call the default implementation
+        if(value.second) {
+            routineHandler = value.second;
+        }
+        else {
+            routineHandler = getDefaultHandler(routineType);
+        }
     }
+
+    if(routineHandler != NULL) {
+        routineHandler();
+    }
+    else{
+        std::cout << "No implementation found for this routine!\n";
+    }
+        
+}
+
+void addDefaultHandlers() {
+    defaults.emplace(MEMORY_ALLOC, defaultAlloc);
+    defaults.emplace(MEMORY_DEALLOC, defaultDealloc);
+    defaults.emplace(SYNCHRONIZATION, defaultSynchronization);
+    defaults.emplace(WRITE_TO_MEMORY, defaultWrite);
+    defaults.emplace(READ_FROM_MEMORY, defaultRead);
 }
 
 int main() {
+
+    // add default handlers
+    addDefaultHandlers();
+    // add 
     addHandlers();
     // since the library doesn't specify any specific actions it invokes the default implementation 
     event_handler("my_free");
